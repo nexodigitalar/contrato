d7fc669cc99bef73b19d7902850c590efbcdc072
diaslucia <luciadias.dev@gmail.com>
2023-02-15 13:35:53 -0300

    Resolve merge

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,18 +1,5 @@
 import "./Input.scss";
 
-<<<<<<< HEAD
-const Input = ({ placeholder, error, type, click }) => {
-  return (
-    <div className="inputContainer">
-      <input
-        type={type}
-        className="input"
-        placeholder={placeholder}
-        onKeyUp={click}
-      />
-      <p className="input_error input_error_hidden">{error}</p>
-    </div>
-=======
 const Input = ({ placeholder, click, name, value }) => {
   return (
     <input
@@ -22,7 +9,6 @@ const Input = ({ placeholder, click, name, value }) => {
       name={name}
       onChange={click}
     />
->>>>>>> validations
   );
 };
 
